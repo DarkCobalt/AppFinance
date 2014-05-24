7ee0818a22c5ad6a2e374049b89048ac69b8bbb0
Łukasz Banasiak <lukasz@banasiak.me>
2014-05-24 15:43:38 +0200

    working chart

--- Djangotest/finance/views.py
+++ Djangotest/finance/views.py
@@ -1,9 +1,22 @@
-from django.http import HttpResponse
-from yahoo_finance import Share
 from django.shortcuts import render
 from forms import Date
 
-# Create your views here.
+import time
+from yahoo_finance import Share
+
+
+def to_chartit_json(json):
+    chartit_json = []
+    for d in json:
+            chartit_json.append([
+                str(int(time.mktime(time.strptime(d['Date'], "%Y-%m-%d")))),
+                float(d['Open']),
+                float(d['High']),
+                float(d['Low']),
+                float(d['Close']),
+            ])
+    chartit_json.reverse()
+    return chartit_json
 
 
 def index(request):
@@ -14,8 +27,9 @@ def index(request):
             date_end = form.cleaned_data['end']
             ticker = form.cleaned_data['tickers']
             share = Share(ticker)
-            respond = share.get_historical(date_start, date_end)
-            return HttpResponse(respond)
+            chart_data = share.get_historical(date_start, date_end)
+            chart_data = to_chartit_json(chart_data)
+            return render(request, 'finance/index.html', {'form': form, 'chart': chart_data})
     else:
         form = Date()
 
